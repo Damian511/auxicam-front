a4d84fd8983c15a79ad94dbdda5a784cff95e485
Damian511 <ang.dam96@gmail.com>
2022-08-08 14:30:56 -0400

    version 08/08

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/auxicam/'
+  : '/',
   pwa: {
     name: 'Auxicam',
     themeColor: '#78909C'
